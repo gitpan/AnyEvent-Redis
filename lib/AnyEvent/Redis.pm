b1776887538527cbbd539b8ed4aa53647af78f66
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-08-25 19:53:27 +0000

    Import of MIYAGAWA/AnyEvent-Redis-0.19_01 from CPAN.
    gitpan-cpan-distribution: AnyEvent-Redis
    gitpan-cpan-version:      0.19_01
    gitpan-cpan-path:         MIYAGAWA/AnyEvent-Redis-0.19_01.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     developer

--- lib/AnyEvent/Redis.pm
+++ lib/AnyEvent/Redis.pm
@@ -2,18 +2,16 @@ package AnyEvent::Redis;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.12';
+our $VERSION = '0.19_01';
 
 use constant DEBUG => $ENV{ANYEVENT_REDIS_DEBUG};
 use AnyEvent;
 use AnyEvent::Handle;
 use AnyEvent::Socket;
 use Try::Tiny;
+use Carp qw(croak);
 
 our $AUTOLOAD;
-
-my %bulk_command = map { $_ => 1 }
-    qw( set setnx rpush lpush lset lrem sadd srem sismember echo getset smove zadd zrem zscore zincrby append hexists hset hsetnx hget hmget hmset hdel);
 
 sub new {
     my($class, %args) = @_;
@@ -73,7 +71,10 @@ sub connect {
 
     $self->{sock} = tcp_connect $self->{host}, $self->{port}, sub {
         my $fh = shift
-            or die "Can't connect Redis server: $!";
+            or do {
+              $cv->croak("Can't connect Redis server: $!");
+              return
+            };
 
         my $hd = AnyEvent::Handle->new(
             fh => $fh,
@@ -97,45 +98,96 @@ sub connect {
                 $cb = pop if ref $_[-1] eq 'CODE';
             }
 
-            my $cv_send = sub {
-                my $cv = shift;
-                my($res, $err) = @_;
-                $self->all_cv->end;
-                $err ? $cv->croak($res) : $cv->send($res);
-            };
-
-            my $send;
-            if ( defined $bulk_command{$command} ) {
-                my $value = pop;
-                $value = '' if ! defined $value;
-                $send = uc($command)
-                      . ' '
-                      . join(' ', @_)
-                      . ' '
-                      . length( $value )
-                      . "\r\n$value\r\n";
-            } else {
-                $send = uc($command)
-                    . ' '
-                    . join(' ', @_)
-                    . "\r\n";
-            }
+            my $send = join("\r\n",
+                  "*" . (1 + @_),
+                  map(('$' . length $_ => $_), uc($command), @_))
+                . "\r\n";
 
             warn $send if DEBUG;
 
             $cv ||= AE::cv;
-            $cv->cb(sub {
-                my $cv = shift;
-                try {
-                    my $res = $cv->recv;
-                    $cb->($res);
-                } catch {
-                    ($self->{on_error} || sub { die @_ })->($_);
-                }
-            }) if $cb;
 
             $hd->push_write($send);
-            $hd->push_read(line => _decoder($send, $cv, $cv_send, $command));
+
+            # Are we already subscribed to anything?
+            if($self->{sub} && %{$self->{sub}}) {
+
+              croak "Use of non-pubsub command during pubsub session may result in unexpected behaviour"
+                unless $command =~ /^p?(?:un)?subscribe$/i;
+
+              # Remember subscriptions
+              $self->{sub}->{$_} ||= [$cv, $cb] for @_;
+
+            } elsif ($command !~ /^p?subscribe$/i) {
+
+                $cv->cb(sub {
+                    my $cv = shift;
+                    try {
+                        my $res = $cv->recv;
+                        $cb->($res);
+                    } catch {
+                        ($self->{on_error} || sub { die @_ })->($_);
+                    }
+                }) if $cb;
+
+                $hd->push_read(ref $self => sub {
+                        my($res, $err) = @_;
+
+                        if($command eq 'info') {
+                          $res = { map { split /:/, $_, 2 } split /\r\n/, $res };
+                        } elsif($command eq 'keys' && !ref $res) {
+                          # Older versions of Redis (1.2) need this
+                          $res = [split / /, $res];
+                        }
+
+                        $self->all_cv->end;
+                        $err ? $cv->croak($res) : $cv->send($res);
+                    });
+
+            } else {
+                croak "Must provide a CODE reference for subscriptions" unless $cb;
+
+                # Remember subscriptions
+                $self->{sub}->{$_} ||= [$cv, $cb] for @_;
+
+                my $res_cb; $res_cb = sub {
+
+                    $hd->push_read(ref $self => sub {
+                            my($res, $err) = @_;
+
+                            if(ref $res) {
+                                my $action = lc $res->[0];
+                                warn "$action $res->[1]" if DEBUG;
+
+                                if($action eq 'message') {
+                                    $self->{sub}->{$res->[1]}[1]->($res->[2], $res->[1]);
+
+                                } elsif($action eq 'pmessage') {
+                                    $self->{sub}->{$res->[1]}[1]->($res->[3], $res->[2], $res->[1]);
+
+                                } elsif($action eq 'subscribe' || $action eq 'psubscribe') {
+                                    $self->{sub_count} = $res->[2];
+
+                                } elsif($action eq 'unsubscribe' || $action eq 'punsubscribe') {
+                                    $self->{sub_count} = $res->[2];
+                                    $self->{sub}->{$res->[1]}[0]->send;
+                                    delete $self->{sub}->{$res->[1]};
+                                    $self->all_cv->end;
+
+                                } else {
+                                    warn "Unknown pubsub action: $action";
+                                }
+                            }
+
+                            if($self->{sub_count} || %{$self->{sub}}) {
+                                # Carry on reading while we are subscribed
+                                $res_cb->();
+                            }
+                        });
+                };
+
+                $res_cb->();
+            }
 
             return $cv;
         };
@@ -144,69 +196,119 @@ sub connect {
             my($cv, @args) = @$queue;
             $self->{cmd_cb}->(@args, $cv);
         }
+
     };
 
     return $cv;
 }
 
-sub _decoder {
-    my ($send, $cv, $cv_send, $command) = @_;
-    sub {
-        my($hd, $result) = @_;
-        warn "got line <$result> for command [$send]" if DEBUG;
-        my $type = substr $result, 0, 1;
-        $result =~ s/^.//;
+sub anyevent_read_type {
+    my(undef, $cb) = @_;
 
-        if ( $type eq '-' ) {
-            $cv_send->($cv, $result, 1);
-        } elsif ( $type eq '+' ) {
-            $cv_send->($cv, $result);
-        } elsif ( $type eq '$' ) {
-            if ($result < 0) {
-                return $cv_send->($cv, undef);
-            }
-            $hd->unshift_read(chunk => $result + 2, sub {
-                        my($hd, $chunk) = @_;
-                        $chunk =~ s/\r\n$//;
-                        warn "chunk <$chunk>" if DEBUG;
-                        if ($command eq 'info') {
-                            my %info = map { split /:/, $_, 2 } split /\r\n/, $chunk;
-                            $cv_send->($cv, \%info);
-                        } elsif ($command eq 'keys') {
-                            my @keys = split /\s+/, $chunk;
-                            $cv_send->($cv, \@keys);
-                        } else {
-                            $cv_send->($cv, $chunk);
-                        }
+    sub {
+        my($hd) = @_;
+
+        return unless defined $hd->{rbuf};
+
+        if($hd->{rbuf} =~ /^[-+:]/) {
+            $hd->{rbuf} =~ s/^([-+:])([^\015\012]*)\015?\012// or return;
+
+            $cb->($2, $1 eq '-');
+
+            return 1;
+
+        } elsif($hd->{rbuf} =~ /^\$/) {
+            $hd->{rbuf} =~ s/^\$([-0-9]+)\015?\012// or return;
+            my $len = $1;
+
+            if($len < 0) {
+                $cb->(undef);
+            } elsif($len + 2 <= length $hd->{rbuf}) {
+                $cb->(substr $hd->{rbuf}, 0, $len);
+                # Remove ending newline
+                substr $hd->{rbuf}, 0, $len + 2, "";
+            } else {
+                $hd->unshift_read (chunk => $len + 2, sub {
+                        $cb->(substr $_[1], 0, $len);
                     });
-        } elsif ( $type eq '*' ) {
-            my $size = $result;
-            warn "size is $size" if DEBUG;
-            if ($result < 0) {
-                return $cv_send->($cv, undef);
-            } elsif ($result == 0) {
-                return $cv_send->($cv, []);
             }
+
+            return 1;
+
+        } elsif($hd->{rbuf} =~ /^\*/) {
+
+            $hd->{rbuf} =~ s/^\*([-0-9]+)\015?\012// or return;
+            my $size = $1;
             my @lines;
-            for (1..$size) {
-                $hd->unshift_read(line => _decoder
-                                      ("recursion $_ of $send", undef,
-                                       sub { my (undef, $result) = @_;
-                                             push @lines, $result;
-                                             if (@lines >= $size) {
-                                                 $cv_send->($cv, \@lines);
-                                             }
-                                         },
-                                       $command
-                                   ));
+
+            my $reader; $reader = sub {
+                my($hd) = @_;
+
+                while(@lines < $size) {
+                    if($hd->{rbuf} =~ /^([\$\-+:])([^\012\015]+)\015?\012/) {
+                        my $type = $1;
+                        my $line = $2;
+
+                        if($type =~ /[-+:]/) {
+                            $hd->{rbuf} =~ s/^[^\012\015]+\015?\012//;
+                            push @lines, $line;
+                        } elsif($line < 0) {
+                            $hd->{rbuf} =~ s/^[^\012\015]+\015?\012//;
+                            push @lines, undef;
+
+                        } elsif(2 + $line <= length $hd->{rbuf}) {
+                            $hd->{rbuf} =~ s/^[^\012\015]+\015?\012//;
+                            push @lines, substr $hd->{rbuf}, 0, $line, "";
+                            $hd->{rbuf} =~ s/^\015?\012//;
+
+                        } else {
+                            # Data not buffered, so we need to do this async
+                            $hd->unshift_read($reader);
+                            return 1;
+                        }
+                    } elsif($hd->{rbuf} =~ /^\*/) { # Nested
+
+                        $hd->unshift_read(__PACKAGE__, sub {
+                                push @lines, $_[0];
+
+                                if(@lines == $size) {
+                                    $cb->(\@lines);
+                                } else {
+                                    $hd->unshift_read($reader);
+                                }
+                                return 1;
+                            });
+                        return 1;
+                    } else {
+                        $hd->unshift_read($reader);
+                    }
+                }
+
+                if($size < 0 || @lines == $size) {
+                    $cb->($size < 0 ? undef : \@lines);
+                    return 1;
+                }
+
+                return;
+            };
+
+            return $reader->($hd);
+
+        } elsif(length $hd->{rbuf}) {
+            # remove extra lines
+            $hd->{rbuf} =~ s/^\015?\012//g;
+
+            if(length $hd->{rbuf}) {
+                # Unknown
+                $cb->("Unknown type", 1);
+                return 1;
             }
-        } elsif ( $type eq ':' ) {
-            $cv_send->($cv, $result);
-        } else {
-            $cv_send->($cv, "Unknown type $type", 1);
         }
+
+        return;
     }
 }
+
 1;
 __END__
 
@@ -244,17 +346,108 @@ AnyEvent::Redis - Non-blocking Redis client
 
 AnyEvent::Redis is a non-blocking Redis client using AnyEvent.
 
-=head1 AUTHOR
+=head1 METHODS
 
-Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+All methods supported by your version of Redis should be supported.
+
+=head2 Normal commands
+
+There are two alternative approaches for handling results from commands.
+
+=over 4
+
+=item * L<AnyEvent::CondVar> based:
+
+  my $cv = $redis->command(
+    # arguments to command
+  );
+
+  $cv->cb(sub {
+    my($cv) = @_;
+    my($result, $err) = $cv->recv
+  });
+
+=item * Callback:
+
+  $redis->command(
+    # arguments,
+    sub {
+      my($result, $err) = @_;
+    });
+
+(Callback is a wrapper around the C<$cv> approach.)
+
+=back
+
+=head2 Subscriptions
+
+The subscription methods (C<subscribe> and C<psubscribe>) must be used with a callback:
+
+  my $cv = $redis->subscribe("test", sub {
+    my($message, $channel[, $actual_channel]) = @_;
+    # ($actual_channel is provided for pattern subscriptions.)
+  });
+
+The C<$cv> condition will be met on unsubscribing from the channel.
+
+Due to limitations of the Redis protocol the only valid commands on a
+connection with an active subscription are subscribe and unsubscribe commands.
+
+=head2 Common methods
+
+=over 4
+
+=item * get
+
+=item * set
+
+=item * hset
+
+=item * hget
+
+=item * lpush
+
+=item * lpop
+
+=back
+
+The Redis command reference
+(L<http://code.google.com/p/redis/wiki/CommandReference>) lists all commands
+Redis supports.
+
+=head1 REQUIREMENTS
+
+This requires Redis >= 1.2.
+
+=head1 COPYRIGHT
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt> 2009-
 
 =head1 LICENSE
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
+=head1 AUTHORS
+
+Tatsuhiko Miyagawa
+
+David Leadbeater
+
+Chia-liang Kao
+
+franck cuny
+
+Lee Aylward
+
+Joshua Barratt
+
+Jeremy Zawodn
+
+Leon Brocard
+
 =head1 SEE ALSO
 
-L<Redis> L<AnyEvent>
+L<Redis>, L<AnyEvent>
 
 =cut
